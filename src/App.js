dda5c28a49f7bdcc1628f95950ec385fd2a3ce05
Gregory Attra <gbattra@gregorys-mbp.home>
2020-03-21 14:18:24 -0400

    rearranged obj keys / updated images

--- src/App.js
+++ src/App.js
@@ -17,115 +17,114 @@ const ME_BLOCK = {
 const PROJECTS = [
   ME_BLOCK,
   {
-    imagePath: 'assets/images/PaintMe.jpg',
-    href: 'https://github.com/gbattra/paint-me-v2',
     header: 'Paint Me',
     subheader: 'NST-based webapp for generating AI art',
+    imagePath: 'assets/images/PaintMe.jpg',
+    href: 'https://github.com/gbattra/paint-me-v2',
     description: '',
     skills: 'Python, Tensorflow, NST, Django, Flask, Pub/Sub, GCP'
   },
   {
-    imagePath: 'assets/images/VAE.png',
-    href: 'https://github.com/gbattra/MNIST_VAE',
     header: 'MNIST VAE',
     subheader: 'A VAE to generate MNIST samples / Tensorflow',
+    imagePath: 'assets/images/Generator.png',
+    href: 'https://github.com/gbattra/MNIST_VAE',
     description: '',
     skills: 'Python, Tensorflow, CNN, VAE'
   },
   {
-    imagePath: 'assets/images/GANs.png',
-    href: 'https://github.com/gbattra/GANsProject',
     header: 'MNIST GANs',
     subheader: 'A first attempt at a GANs model using Tensorflow',
+    imagePath: 'assets/images/GANs.png',
+    href: 'https://github.com/gbattra/GANsProject',
     description: '',
     skills: 'Python, Tensorflow, CNN, GANs'
   },
   {
-    imagePath: 'assets/images/ML.jpg',
-    href: 'https://github.com/gbattra/ML_Framework',
     header: 'ML Framework',
     subheader: 'A machine learning framework hacked together with Numpy',
+    imagePath: 'assets/images/ML.jpg',
+    href: 'https://github.com/gbattra/ML_Framework',
     description: '',
     skills: 'Python, Numpy, Linear & Logistic Regression'
   },
   {
-    imagePath: 'assets/images/SML.png',
-    href: 'https://github.com/gbattra/machine_learning',
     header: 'Stanford Machine Learning Projects',
     subheader: 'Projects completed for the Stanford Machine Learning MOOC',
+    imagePath: 'assets/images/SML.png',
+    href: 'https://github.com/gbattra/machine_learning',
     description: '',
     skills: 'Matlab, NN, SVM, KMeans, PCA'
   },
   {
-    imagePath: 'assets/images/Generator.png',
-    href: 'https://github.com/gbattra/Generator_ML_Framework',
     header: 'Generator ML Framework',
     subheader: 'POC for Generative AI algorithm',
+    imagePath: 'assets/images/spongebob.png',
+    href: 'https://github.com/gbattra/Generator_ML_Framework',
     description: '',
     skills: 'Python, Numpy, OOP, Linear Algebra'
   },
-
   {
-    imagePath: 'assets/images/Streetview.png',
-    href: 'https://github.com/gbattra/StreetviewHouseNumberDetector',
     header: 'Street View House Number Detector',
     subheader: 'CNN for street-view house number detection and classification',
+    imagePath: 'assets/images/Streetview.png',
+    href: 'https://github.com/gbattra/StreetviewHouseNumberDetector',
     description: '',
     skills: 'Unity, C#, Linear Algebra, 3D Graphics'
   },
   {
-    imagePath: 'assets/images/NN.png',
-    href: 'https://github.com/gbattra/NeuralNetwork',
     header: 'Simple Neural Network',
     subheader: '3 layer neural network to predict presence and type of Arrhythmia in patients',
+    imagePath: 'assets/images/NN.png',
+    href: 'https://github.com/gbattra/NeuralNetwork',
     description: '',
     skills: 'Python, Numpy, Pandas, Matplotlib'
   },
   {
-    imagePath: 'assets/images/LogisticRegression.png',
-    href: 'https://github.com/gbattra/LogisticRegression',
     header: 'Simple Logisitc Regression',
     subheader: 'A logistic regression model to classify types of cancer and skin conditions',
+    imagePath: 'assets/images/LogisticRegression.png',
+    href: 'https://github.com/gbattra/LogisticRegression',
     description: '',
     skills: 'Python, Numpy, Pandas, Matplotlib'
   },
   {
-    imagePath: 'assets/images/LinearRegression.png',
-    href: 'https://github.com/gbattra/LinearRegression',
     header: 'Simple Linear Regression',
     subheader: 'A linear regression model for bike rentalss',
+    imagePath: 'assets/images/LinearRegression.png',
+    href: 'https://github.com/gbattra/LinearRegression',
     description: '',
     skills: 'Python, Numpy, Pandas, Matplotlib'
   },
   {
-    imagePath: 'assets/images/phaser.png',
-    href: 'https://github.com/gnwgames/CircleDodge',
     header: 'CircleDodge',
     subheader: 'A PhaserJS game where you dodge circles',
+    imagePath: 'assets/images/phaser.png',
+    href: 'https://github.com/gnwgames/CircleDodge',
     description: '',
     skills: 'Javascript, PhaserJS, Game AI'
   },
   {
-    imagePath: 'assets/images/react.png',
-    href: 'https://github.com/gbattra/baxer_client',
     header: 'Baxer',
     subheader: 'React frontend for a music sharing application',
+    imagePath: 'assets/images/react.png',
+    href: 'https://github.com/gbattra/baxer_client',
     description: '',
     skills: 'Javascript, React, HTML/CSS, Semantic UI'
   },
   {
-    imagePath: 'assets/images/rails.png',
-    href: 'https://github.com/gbattra/rocketyr-api',
     header: 'Roketyr',
     subheader: 'Ruby on Rails backend for a music sharing application',
+    imagePath: 'assets/images/rails.png',
+    href: 'https://github.com/gbattra/rocketyr-api',
     description: '',
     skills: 'Ruby, Rails, rSpec, MVC, OOP'
   },
   {
-    imagePath: 'assets/images/hexdoge.jpg',
-    href: 'https://github.com/gbattra/HexDodge',
     header: 'HexDodge',
     subheader: 'iOS game built in Unity',
+    imagePath: 'assets/images/hexdoge.jpg',
+    href: 'https://github.com/gbattra/HexDodge',
     description: '',
     skills: 'Unity, C#, Linear Algebra, 3D Graphics'
   }
